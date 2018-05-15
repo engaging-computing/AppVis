bee19b96d1208c33e66a13826ffe84f5b7310162
christopher <christopher.r.odom@gmail.com>
2018-05-15 14:35:39 -0400

    Wrote out basic values to extend propertyeditor

--- appinventor/components/src/edu/uml/cs/isense/ISenseVisPropertyEditor.java
+++ appinventor/components/src/edu/uml/cs/isense/ISenseVisPropertyEditor.java
@@ -47,5 +47,30 @@ package edu.uml.cs.isense;
 import com.google.appinventor.client.Ode.MESSAGES;
 import com.google.appinventor.client.widgets.properties.ChoicePropertyEditor;
 
-public class ISenseVisPropertyEditor{
+/**
+ * Property editor for appvis vis types
+ *
+ * @author Christopher Odom christopher.r.odom@gmail.com
+ */
+
+public class ISenseVisPropertyEditor extends ChoicePropertyEditor{
+  private static final Choice[] vis{
+    new Choice(MESSAGES.defaultISenseChoice(), "0"),
+    new Choice(MESSAGES.mapISenseChoice(), "1"),
+    new Choice(MESSAGES.timelineISenseChoice(), "2"),
+    new Choice(MESSAGES.scatterISenseChoice(), "3"),
+    new Choice(MESSAGES.barISenseChoice(), "4"),
+    new Choice(MESSAGES.histogramISenseChoice(), "4"),
+    new Choice(MESSAGES.boxISenseChoice(), "6"),
+    new Choice(MESSAGES.pieISenseChoice(), "7"),
+    new Choice(MESSAGES.tableISenseChoice(), "8"),
+    new Choice(MESSAGES.summaryISenseChoice(), "9"),
+    new Choice(MESSAGES.photoISenseChoice(), "10"),
+  };
+
+  ISenseVisPropertyEditor(){
+    super(vis);
+  }
+
+}
   
