ed2f9c1f3daac2b0378c8cb5a99a8cae579fca24
christopher <christopher.r.odom@gmail.com>
2018-06-01 10:46:55 -0400

    Used a Uri constructor rather than a URI conversion

--- appinventor/components/src/edu/uml/cs/isense/iSENSEPublisher.java
+++ appinventor/components/src/edu/uml/cs/isense/iSENSEPublisher.java
@@ -306,10 +306,10 @@ public final class iSENSEPublisher extends AndroidNonvisibleComponent implements
         }
       } else if (pathtokens[0].equals("content:")) {
         try {
-          Cursor cursor;
+          Cursor cursor = null;
           try {
             String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = activity.getContentResolver().query(new File(Photo).toURI(), proj, null, null, null);
+            cursor = activity.getContentResolver().query(Uri.fromFile(new File(Photo)), proj, null, null, null);
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             cursor.moveToFirst();
             path = cursor.getString(column_index);
